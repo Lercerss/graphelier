a47c84a031339f3ce46dbb7f2a278a11eb394c51
chris-baur <chriskbaur@gmail.com>
2019-09-23 17:12:42 -0400

    added helper method for timestamp

---
+++ app/src/utils/helpers.js
@@ -0,0 +1,21 @@
+export default const getFormattedDate = () => {
+    let date = new Date();
+
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getMilliseconds()
+
+    month = (month < 10 ? '0' : '') + month;
+    day = (day < 10 ? '0' : '') + day;
+    hour = (hour < 10 ? '0' : '') + hour;
+    min = (min < 10 ? '0' : '') + min;
+    // sec = (sec < 10 ? '0' : '') + sec;
+
+    let str = date.getFullYear() + '-' + month + '-' + day + '_' +  hour + ':' + min + ':' + sec;
+
+    /*alert(str);*/
+
+    return str;
+}
