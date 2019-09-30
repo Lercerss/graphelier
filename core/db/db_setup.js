ce5721daf8cc142f161f915ff0bafe86dc1a6ba7
Chirac <chiracmanoukian@hotmail.com>
2019-09-30 16:29:45 -0400

    [#7] Data importer now populates mongo DB
    - Wrote a dockerfile for mongo to initialize the db
    
    IMPORTANT:
    - Added a new parameter to record the instrument of the orderbook

---
+++ core/db/db_setup.js
@@ -0,0 +1,8 @@
+conn = new Mongo();
+db = conn.getDB('graphelier-db');
+
+db.createCollection('orderbooks');
+db.createCollection('messages');
+
+db.orderbooks.createIndex({ instrument: 1, timestamp: 1 });
+db.messages.createIndex({ instrument: 1, timestamp: 1 });
