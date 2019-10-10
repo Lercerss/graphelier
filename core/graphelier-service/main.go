1708f645093895c45549f9b36ec0d5d30017fb1c
pvacca97 <patrick.vacca@hotmail.com>
2019-10-10 18:44:16 -0400

    [#41]commit to keep changes for moving project, started refactor

--- core/graphelier-service/main.go
+++ core/graphelier-service/main.go
@@ -4,15 +4,15 @@ import (
 	"log"
 	"net/http"
 
-	"graphelier/core/graphelier-service/cnxn"
+	"graphelier/core/graphelier-service/models"
 	"graphelier/core/graphelier-service/rqst"
 
 	"github.com/gorilla/handlers"
 )
 
 func main() {
-	db := cnxn.GetInstance()
-	db.Connect()
+	// db := cnxn.GetInstance()
+	// db.Connect()
 
 	r := rqst.NewRouter()
 	log.Fatal(http.ListenAndServe(":5050", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(r)))
