f3072eedaa9899e864ed942a47b8adfdb2239254
panoreak <panorea.kontis@gmail.com>
2019-10-14 00:51:19 -0400

    [#39] Made changes requested by alvyn

--- app/src/components/OrderBookSnapshot.js
+++ app/src/components/OrderBookSnapshot.js
@@ -7,7 +7,7 @@ import classNames from 'classnames';
 
 
 import OrderBookService from '../services/OrderBookService';
-import {SNAPSHOT_INSTRUMENT} from '../constants/Constants';
+import {SNAPSHOT_INSTRUMENT, NANOSECONDS_IN_NINE_AND_A_HALF_HOURS, NANOSECONDS_IN_SIXTEEN_HOURS} from '../constants/Constants';
 
 
 class OrderBookSnapshot extends Component {
@@ -44,16 +44,29 @@ class OrderBookSnapshot extends Component {
         }
     }
 
+
+    /**
+     * Handles the date change for the TextField date picker
+     *
+     * @param event The event object that caused the call
+     */
     handleChangeDate = (event) => {
-        var selectedDateNano = parseInt(dateStringToEpoch(event.target.value + ' 00:00:00'));
+        let selectedDateNano = parseInt(dateStringToEpoch(event.target.value + ' 00:00:00'));
 
         this.setState({ 
             selectedDateNano: selectedDateNano,
         }, () => {this.handleChangeDateTime();});
     }
 
+    /**
+     * Handles the time change when sliding the time Slider
+     *
+     * @param event The event object that caused the call
+     * 
+     * @param value The new time value that represents the nanoseconds between 12 am and the chosen time of day.
+     */
     handleChangeTime = (event, value) => {
-        var selectedTimeNano = parseInt(value);
+        let selectedTimeNano = parseInt(value);
 
         this.setState({ 
             selectedTimeNano: selectedTimeNano,
@@ -61,8 +74,14 @@ class OrderBookSnapshot extends Component {
         });
     }
 
+    /**
+     * Handles the time change when the user stops sliding the time Slider
+     *
+     * @param event The event object that caused the call
+     * @param value The new time value that represents the nanoseconds between 12 am and the chosen time of day
+     */
     handleCommitTime = (event, value) => {
-        var selectedTimeNano = parseInt(value);
+        let selectedTimeNano = parseInt(value);
 
         this.setState({ 
             selectedTimeNano: selectedTimeNano,
@@ -70,9 +89,12 @@ class OrderBookSnapshot extends Component {
         }, () => {this.handleChangeDateTime();});
     }
 
+    /**
+     * Updates the selectedDateTimeNano state variable when there is a change in the date or when the user stops sliding the time Slider
+     */
     handleChangeDateTime = () => {
         const {selectedDateNano, selectedTimeNano} = this.state;
-        var selectedDateTimeNano = selectedTimeNano+selectedDateNano;
+        let selectedDateTimeNano = selectedTimeNano+selectedDateNano;
 
             this.setState({ 
                 selectedDateTimeNano: selectedDateTimeNano,
@@ -87,59 +109,58 @@ class OrderBookSnapshot extends Component {
                 maxWidth={'xl'}
                 component={'div'}
                 className={classes.root}>
-                <Typography component='div' className={classes.container}>
-                    <div id='ButtonHeader' className={classes.divTopBook}>
+                <Typography component={'div'} className={classes.container}>
+                    <div id={'ButtonHeader'} className={classes.divTopBook}>
                         <FormControl className={classes.formControl}>
                             <div className={classes.inline}>
                                 <Typography
-                                    variant='body1'
+                                    variant={'body1'}
                                     className={classNames(classes.inputLabel, classes.alignEnd)}
-                                    color='textSecondary'>
+                                    color={'textSecondary'}>
                                     {'Date'}  
                                 </Typography>
                                 <TextField 
                                     className={classes.datePicker}
-                                    id='time'
-                                    type='date'
+                                    type={'date'}
                                     onChange={this.handleChangeDate}
                                 />
                             </div>
                             <div className={classes.inline}>
                                 <Typography
-                                    variant='body1'
+                                    variant={'body1'}
                                     className={classes.inputLabel}
-                                    color='textSecondary'>
+                                    color={'textSecondary'}>
                                     {'Time'}  
                                 </Typography>
                                 <Typography 
-                                    variant='body1'
+                                    variant={'body1'}
                                     className={classes.timestampDisplay}>
                                     {this.state.selectedTimeString}
                                 </Typography>
                             </div>
                             <Slider
                                 className={classes.timestampSlider}
-                                min={9.5*60*60*1000000000} // nanoseconds between 12am and 9:30am
-                                max={16*60*60*1000000000} // nanoseconds between 12am and 4pm
+                                min={NANOSECONDS_IN_NINE_AND_A_HALF_HOURS} // nanoseconds between 12am and 9:30am
+                                max={NANOSECONDS_IN_SIXTEEN_HOURS} // nanoseconds between 12am and 4pm
                                 step={1}
                                 defaultValue={0}
                                 onChange={this.handleChangeTime}
                                 marks={[
                                     {
-                                        value: 9.5*60*60*1000000000,
+                                        value: NANOSECONDS_IN_NINE_AND_A_HALF_HOURS,
                                         label: '9:30AM',
                                     },
                                     {
-                                        value: 16*60*60*1000000000,
+                                        value: NANOSECONDS_IN_SIXTEEN_HOURS,
                                         label: '4:00PM',
                                     },
                                 ]}
                                 onChangeCommitted={this.handleCommitTime}
                             />
-                            {this.state.selectedDateTimeNano === 0 && 
+                            {(this.state.selectedTimeNano === 0 || this.state.selectedDateNano === 0 ) && 
                                 <Typography 
-                                    variant='body1'
-                                    color='error'
+                                    variant={'body1'}
+                                    color={'error'}
                                     className={classes.selectMessage}>
                                     Please select a date and time
                                 </Typography>
