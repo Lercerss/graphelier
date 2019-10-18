dc61eda37ae44ef1128d6ed04f7076bf22cfd904
chris-baur <chriskbaur@gmail.com>
2019-10-18 16:01:13 -0400

    changed styling [#40]

--- app/src/styles/OrderBookSnapshot.js
+++ app/src/styles/OrderBookSnapshot.js
@@ -2,30 +2,33 @@ import {Colors} from './App';
 
 export const Styles = {
     root:{
-        backgroundColor: Colors.empty,
+        backgroundColor: 'transparent',
         minHeight: '100vh',
     },
     container: {
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: 'transparent',
         fontSize: '1.5rem',
         paddingBottom: 20,
     },
     divTopBook: {
         display: 'flex',
         justifyContent: 'flex-end',
-        backgroundColor: Colors.empty
+        backgroundColor: 'transparent'
     },
     NativeSelect: {
         margin: '1.5rem'
     },
     divTopBookBody: {
+        backgroundColor: `${Colors.lightBlue} !important`,
         display: 'flex',
         justifyContent: 'center',
         padding: '1rem'
     },
     formControl: {
         minWidth: 350,
-        marginTop: 20,
         marginBottom: 20,
     },
+    transparentBackground: {
+        backgroundColor: 'transparent'
+    }
 };
