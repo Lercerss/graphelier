15cbb508b016e21731784504e3ef407dc33eeff3
chris-baur <chriskbaur@gmail.com>
2019-10-24 19:49:20 -0400

    removing commented line #40

--- app/src/services/OrderBookService.js
+++ app/src/services/OrderBookService.js
@@ -4,6 +4,5 @@ export default class OrderBookService {
 
     static getOrderBookPrices = (instrument, timestamp) => {
         return httpClient.get(`/orderbook/${instrument}/${timestamp}`);
-        //return httpClient.get(`/orderbook/SPY/1340285560000000000`);
     };
 };
