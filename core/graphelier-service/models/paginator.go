2346af03a757920b43aeb7ee3118f8364f559ff6
Chirac <chiracmanoukian@hotmail.com>
2019-11-09 23:43:43 -0500

    [#95] Big numbers are now converted to strings
    - This is needed to keep the number's precision

--- core/graphelier-service/models/paginator.go
+++ core/graphelier-service/models/paginator.go
@@ -3,5 +3,5 @@ package models
 // Paginator contains info about the page of data being requested
 type Paginator struct {
 	NMessages int64 `json:"nMessages"`
-	SodOffset int64 `json:"sod_offset"`
+	SodOffset int64 `json:"sod_offset,string"`
 }
