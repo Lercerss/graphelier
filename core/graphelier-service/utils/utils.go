4f4d25ec7adb9d5ee05b7c4f8ae2465dc087acde
Lucas Turpin <34099471+Lercerss@users.noreply.github.com>
2020-01-21 12:25:00 -0500

    [#134] Timer parameter to string
    Also added missing comments

--- core/graphelier-service/utils/utils.go
+++ core/graphelier-service/utils/utils.go
@@ -14,18 +14,21 @@ func Abs(n int64) int64 {
 	return n
 }
 
-func DebugTimer(call interface{}) func() {
+// DebugTimer : Returns a deferrable function to log (at DEBUG level) the amount of time taken in the given scope
+func DebugTimer(call string) func() {
 	start := time.Now()
 	return func() {
 		log.Debugf("%s took %v\n", call, time.Since(start))
 	}
 }
 
-func TraceTimer(call interface{}) func() {
+// TraceTimer : Returns a deferrable function to log (at TRACE level) the amount of time taken in the given scope
+func TraceTimer(call string) func() {
 	start := time.Now()
 	return func() {
 		log.Tracef("%s took %v\n", call, time.Since(start))
 	}
 }
 
+// Timer : See DebugTimer
 var Timer func(interface{}) func() = DebugTimer
