6e3e150ef74b95182a9684597bf6b9024cee8e7f
panoreak <panorea.kontis@gmail.com>
2020-01-22 23:59:44 -0500

    [#108] Moved graph to its own card in OrderBookSnapshot, changed text of message that appears on page load

--- app/src/components/TopOfBookGraphWrapper.tsx
+++ app/src/components/TopOfBookGraphWrapper.tsx
@@ -6,17 +6,16 @@ import { Styles } from '../styles/TopOfBookGraphWrapper';
 
 function TopOfBookGraphWrapper(props) {
     const {
-        classes, onTimeSelect, selectedDateTimeNano, topOfBookItems,
+        onTimeSelect, selectedDateTimeNano, topOfBookItems,
     } = props;
     const [ref, { width, height }] = useDimensions();
     return (
-        <div className={classes.graph}>
+        <div>
             <div
                 ref={ref}
                 style={{ width: '100%', height: 350 }}
             >
                 <TopOfBookGraph
-                    className={classes.graph}
                     height={height}
                     width={width}
                     onTimeSelect={onTimeSelect}
