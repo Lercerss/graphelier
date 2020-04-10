cf142c0d6b604d72ccd274dcd964a6452ece433c
Alvyn Duy-Khoi Le <alvyn279@gmail.com>
2020-04-10 16:23:52 -0400

    [#201] Fix for exposed port on prod for graphelier news

--- news/service/server.py
+++ news/service/server.py
@@ -1,11 +1,9 @@
 import flask
 
-NEWS_SERVER_PORT = 8080
-
 app = flask.Flask(__name__)
 
 with app.app_context():
     import views  # pylint: disable=W0611
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=NEWS_SERVER_PORT)
+    app.run()
